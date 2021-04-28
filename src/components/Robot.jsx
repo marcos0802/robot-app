dba22d90153b79e965577e4af44127c94bd20413
root <root@localhost.localdomain>
2021-04-28 11:31:51 +0100

    Robot Info

--- src/components/Robot.jsx
+++ src/components/Robot.jsx
@@ -1,84 +1,80 @@
-import { useState } from "react";
+import Modal from "react-modal";
+import React from "react";
+import Button from "./Button";
 
 const Robot = ({ robot }) => {
-  const [showDetail, setShowDetail] = useState(false);
+  Modal.setAppElement("#root");
   const imgUrl = `https://robohash.org/${robot.id}`;
+  const [showModal, setShowModal] = React.useState(false);
+  function openModal() {
+    setShowModal(true);
+  }
+
   return (
-    <div className="card" onClick={() => setShowDetail(!showDetail)}>
-      {showDetail ? (
+    <div className="card" id="card">
+      <Modal
+        isOpen={showModal}
+        onRequestClose={() => setShowModal(!showModal)}
+        contentLabel="Example Modal"
+      >
         <div className="robot-detail">
-          <h3>Robot Detail</h3>
-          <div></div>
-          <h4>
-            <span>Name</span>:{robot.name}
-          </h4>
-          <h4>
-            <span>Username</span>: {robot.username}
-          </h4>
-          <h4>
-            <span>Email</span>: {robot.email}
-          </h4>
-          <h4>Address</h4>
-          <h5 style={{ fontSize: "11px" }}>
-            {" "}
-            <span style={{ fontStyle: "italic" }}> City: </span>{" "}
-            {robot.address.city}
-          </h5>
-          <h5 style={{ fontSize: "11px" }}>
-            {" "}
-            <span style={{ fontStyle: "italic" }}> Street: </span>
-            {robot.address.street}{" "}
-          </h5>
-          <h5 style={{ fontSize: "11px" }}>
-            {" "}
-            <span style={{ fontStyle: "italic" }}> Suite: </span>
-            {robot.address.suite}{" "}
-          </h5>
-          <h4>
-            <span>Phone</span>: {robot.phone}
-          </h4>
-          <h4>
-            <span>Website</span>: {robot.website}
-          </h4>
-          <h4>Company</h4>
-          <h5 style={{ fontSize: "11px" }}>
-            {" "}
-            <span style={{ fontStyle: "italic" }}>Name: </span>{" "}
-            {robot.company.name}
-          </h5>
-          <h5 style={{ fontSize: "11px" }}>
-            <span style={{ fontStyle: "italic" }}>Catch Phrase: </span>
-            {robot.company.catchPhrase}{" "}
-          </h5>
-          <h5 style={{ fontSize: "11px" }}>
-            {" "}
-            <span style={{ fontStyle: "italic" }}>Bs: </span>
-            {robot.company.bs}{" "}
-          </h5>
-          <span
-            style={{
-              textWeight: "bold",
-              fontSize: "10px",
-              color: "darkblue",
-              textAlign: "center",
-              cursor:"pointer"
-            }}
-          >
-            <br />
-            Click to see Profile
-          </span>
+          <div className="col robot-detail-image">
+            <img src={imgUrl} alt="User" />
+          </div>
+          <div className="col robot-detail-info">
+            <h4>
+              <span>Name:</span>
+              {robot.name}
+            </h4>
+            <h4>
+              <span>Userame:</span>
+              {robot.username}
+            </h4>
+            <h4>
+              <span>Email:</span>
+              {robot.email}
+            </h4>
+            <h4>
+              <span>Address:</span>
+            </h4>
+            <h4>
+              <span>- City:</span> {robot.address.city}
+              <span> - Street:</span> {robot.address.street}
+              <span> - Suite:</span> {robot.address.suite}
+              <br/>
+               <span> - Lat:</span> {robot.address.geo.lat}
+              <span> - Lng:</span> {robot.address.geo.lng}
+            </h4>
+             <h4>
+              <span>Phone:</span>
+              {robot.phone}
+            </h4>
+             <h4>
+              <span>Website:</span>
+              {robot.website}
+            </h4>
+             <h4>
+              <span>Company:</span>
+            </h4>
+            <h4>
+              <span>- Name:</span> {robot.company.name}
+              <span> - Catch Phrase:</span> {robot.company.catchPhrase}
+              <span> - bs:</span> {robot.company.bs}
+            </h4>
+            <Button
+              color="crimson"
+              text="Close"
+              onClick={() => setShowModal(!showModal)}
+            />
+          </div>
         </div>
-      ) : (
-        <>
-          {" "}
-          <img src={imgUrl} alt="User" />
-          <h3>{robot.name}</h3>
-          <h4>{robot.email}</h4>
-          <span style={{ textWeight: "bold", fontSize: "10px",color:"darkblue" , cursor:"pointer"}}>
-           Click to see Details
-          </span>
-        </>
-      )}
+      </Modal>
+      <div>
+        <img src={imgUrl} alt="User" className="profile-image" />
+        <h3>{robot.name}</h3>
+        <h4>{robot.email}</h4>
+        <Button color="#0ccac4" text="See Details" onClick={openModal} />
+      </div>
     </div>
   );
 };
